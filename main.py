5094129493396d0399d12f1f837d76f71092c4e5
MaSi-HTL <markus.signitzer@htlinn.ac.at>
2024-05-01 21:26:31 +0200

    added user input for two number adding

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ import foo
 
 print("Hello World, hello Github")
 foo.hello()
+a = float(input("pls enter a number ->"))
+b = float(input("pls enter another number ->"))
+print(f"The sum of your numbers {a} + {b} = {foo.add(a,b)}")
+
